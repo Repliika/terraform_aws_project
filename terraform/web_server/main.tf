a56b1bb798e55c1b380afab2f6253a0579dd4e66
Repliika <72854655+Repliika@users.noreply.github.com>
2021-04-08 12:07:14 -0400

    Add files via upload

---
+++ terraform/web_server/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "ginoo"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-042e8287309f5df03"
+  instance_type = "t2.micro"
+  key_name      = "gino-key"
+
+  tags = {
+    Name = var.webserver_name
+  }
+}
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-042e8287309f5df03"
+  instance_type = "t2.micro"
+  key_name      = "gino-key"
+
+  tags = {
+    Name = var.jenkins_name
+  }
+}
---
+++ terraform/web_server/variables.tf
@@ -0,0 +1,11 @@
+variable "webserver_name" {
+
+  description = "name your webserver instance"
+
+}
+
+variable "jenkins_name" {
+
+  description = "name your Jenkins instance"
+
+}
